648e37ac232ab639a8f5df0803ae225606fa7711
Miguel Calo <miguelc@codev.com>
2025-08-27 15:45:14 +0800

    wip: base canvas component

---
+++ src/types.d.ts
@@ -0,0 +1,13 @@
+export interface OrderedPair {
+  x: number;
+  y: number;
+}
+
+export interface DeltaTime {
+  current: number;
+}
+
+export interface Dimension {
+  width: number;
+  height: number;
+}
